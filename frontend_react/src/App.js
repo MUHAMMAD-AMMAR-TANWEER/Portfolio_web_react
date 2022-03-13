482f98d7ff6d4e2ebd91d3f841f191b37387b144
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2022-03-13 10:22:59 +0500

    set the folder structure

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+import { About, Testimonial, Footer, Header, Work, Skills } from './container';
+import {Navbar} from './components';
+import './App.scss';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+        <Navbar/>
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonial />
+        <Footer />
+    </div>
   )
 }
 
