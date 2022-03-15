294e30b428b64f54f35165cdb5c50a5feb06309b
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2022-03-15 06:49:38 +0500

    changes in navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React , { useState }from 'react';
 
 import './Navbar.scss';
 
 import {images} from '../../constants';
+import { motion } from 'framer-motion';
+
+import {HiMenuAlt4, HiX} from 'react-icons/hi';
 
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -22,6 +26,25 @@ const Navbar = () => {
 
         ))}
       </ul>
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4  onClick={() => setToggle(true)}/>
+        {toggle && (
+          <motion.div>
+            whileInView={{x:[300,0]}}
+            transition={{duration: 0.85, ease: 'easeOut'}}
+            <HiX onClick={() => setToggle(false)}/>
+            {['home','about','contact','work','skills'].map((item)=> (
+
+                <li  key={item}>
+                <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+
+                 </li>
+
+            ))}
+          </motion.div>
+        )}
+
+      </div>
     </nav>
   )
 }
